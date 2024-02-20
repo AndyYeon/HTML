34e736a2dd7a8419f2b2f50998c3bb943eb622c2
Soomin <99doselca@gmail.com>
2024-02-20 16:08:57 +0900

    02-20

--- Js/callback.js
+++ Js/callback.js
@@ -1,1 +1,24 @@
 //콜백함수 
+function checkMood(mood,good,bad){
+    if(mood === "good"){
+        //기분 좋을 때 동작
+        good();
+    } else{
+        //기분이 안좋을 때
+        bad();
+    }
+}
+
+function cry(){
+    console.log("Action :: CRY");
+}
+
+function sing(){
+    console.log("Action :: Sing");
+}
+
+function dance(){
+    console.log("ACtion :: DANCE");
+}
+
+checkMood("good",cry,sing);
